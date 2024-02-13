26fee5f73a45aabc5df80f1f2399fa092a6260ed
Lucas G. Reinert <lucasreinert96@gmail.com>
2024-02-13 16:26:48 -0300

    v.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// src/App.js
-import React from 'react';
 import './styles/App.css';
 import Header from './components/Header/Header';
 import MainContainer from './components/MainContainer/MainContainer';

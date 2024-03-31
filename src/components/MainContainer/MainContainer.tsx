bc97293502ba13c303b9cd0b2c901ced8bc3f090
Lucas G. Reinert <lucasreinert96@gmail.com>
2024-03-31 16:32:36 -0300

    language lector fixed

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
+import { Trans } from 'react-i18next';
 import './MainContainer.css';
 import rightImage from '../../assets/right.png';
 import okImage from '../../assets/ok.png';
 import loveImage from '../../assets/love.png';
 
-const MainContainer = () => {
+const MainContainer: React.FC = () => {
   return (
     <div className="main-container">
       <div className="bio">
         <div className="bio-section" id="par1">
           <img src={rightImage} alt="Right" className="bio-img left" />
-          <p>Me chamo <span className='emphasis'>Lucas G. Reinert</span> e sou o <span className='emphasis'>Full-Stack Developer</span> que você precisa!</p>
+          <p>
+            <Trans i18nKey="greeting">
+              Me chamo <span className='emphasis'>Lucas G. Reinert</span> e sou o <span className='emphasis'>Full-Stack Developer</span> que você precisa!
+            </Trans>
+          </p>
         </div>
         <div className="bio-section" id="par2">
-          <p>Com mais de <span className='emphasis'>04 anos de experiência</span> em desenvolvimento de sites, apps e aplicações desktop, sou capaz de criar projetos funcionais, escaláveis e de fácil manutenção.</p>
+          <p>
+            <Trans i18nKey="experience">
+              Com mais de <span className='emphasis'>04 anos de experiência</span> em desenvolvimento de sites, apps e aplicações desktop, sou capaz de criar projetos funcionais, escaláveis e de fácil manutenção.
+            </Trans>
+          </p>
           <img src={okImage} alt="OK" className="bio-img right" />
         </div>
         <div className="bio-section" id="par3">
           <img src={loveImage} alt="Love" className="bio-img left" />
-          <p>Amo meu trabalho e minha maior inspiração é lhe ajudar a <span className='emphasis'>alcançar seus objetivos!</span></p>
+          <p>
+            <Trans i18nKey="passion">
+              Amo meu trabalho e minha maior inspiração é lhe ajudar a <span className='emphasis'>alcançar seus objetivos!</span>
+            </Trans>
+          </p>
         </div>
       </div>
     </div>
--- src/components/anims/LanguageSelector.tsx
+++ src/components/anims/LanguageSelector.tsx
@@ -1,49 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import TestingMenuAnim from './TestingMenuAnim';
 import brIcon from '../../assets/br.png';
 import usIcon from '../../assets/us.png';
 import './LanguageSelector.css';
 
-interface Translations {
-  [key: string]: string;
-}
-
 const LanguageSelector: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const { i18n } = useTranslation();
+  const isLoading = i18n.isInitialized && i18n.language !== 'en';
 
-  const translateContent = async () => {
-    setIsLoading(true);
-    const htmlContent = document.documentElement.outerHTML;
-
-    try {
-      const response = await fetch('https://translaterum-production.up.railway.app/api/translate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ html: htmlContent }),
-      });
-
-      if (!response.ok) throw new Error('Falha na tradução');
-      const translations: Translations = await response.json();
-
-      Object.entries(translations).forEach(([original, translated]) => {
-        const escapedOriginal = original.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        const regex = new RegExp(escapedOriginal, "g");
-        document.body.innerHTML = document.body.innerHTML.replace(regex, translated);
-      });
-
-    } catch (error) {
-      console.error('Erro ao traduzir:', error);
-    } finally {
-      setIsLoading(false);
-    }
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
   };
 
   return (
     <div className='LanguageSelector'>
       {isLoading && <TestingMenuAnim />}
-      <img src={usIcon} alt="Translate to English" onClick={translateContent} />
+      <img src={usIcon} alt="Translate to English" onClick={() => changeLanguage('en')} />
       <img src={brIcon} alt="Translate to Portuguese" onClick={() => window.location.reload()} />
     </div>
   );
